6f1baffac1bf13a10eeace294f8c8abd47aad897
Ronaldo Veloso Filho <ronaldovf24@gmail.com>
2026-03-13 19:16:32 -0300

    feat: criação da entidade Paciente

--- src/main/java/com/fiap/ec/backend_consultas/model/Paciente.java
+++ src/main/java/com/fiap/ec/backend_consultas/model/Paciente.java
@@ -1,9 +1,6 @@
 package com.fiap.ec.backend_consultas.model;
-
 import jakarta.persistence.*;
-
 import java.time.LocalDate;
-
 @Entity
 @Table(name = "pacientes")
 public class Paciente {
@@ -12,69 +9,35 @@ public class Paciente {
     private Long id;
     @Column(nullable = false)
     private String nome;
-    @Column(nullable = false)
-    private String email;
     @Column(nullable = false, unique = true)
     private String cpf;
+    @Column(nullable = false)
+    private String email;
     private String telefone;
-    @Column(nullable = false)
     private LocalDate dataNascimento;
-    @Column(nullable = false)
     private Boolean ativo;
-
-    public Long getId() {
-        return id;
+    public Paciente() {
     }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
+    public Paciente(String nome, String cpf, String email,
+                    String telefone, LocalDate dataNascimento, Boolean ativo) {
         this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
+        this.cpf = cpf;
         this.email = email;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
         this.telefone = telefone;
-    }
-
-    public LocalDate getDataNascimento() {
-        return dataNascimento;
-    }
-
-    public void setDataNascimento(LocalDate dataNascimento) {
         this.dataNascimento = dataNascimento;
-    }
-
-    public Boolean getAtivo() {
-        return ativo;
-    }
-
-    public void setAtivo(Boolean ativo) {
         this.ativo = ativo;
     }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
+    public Long getId() { return id; }
+    public String getNome() { return nome; }
+    public String getCpf() { return cpf; }
+    public String getEmail() { return email; }
+    public String getTelefone() { return telefone; }
+    public LocalDate getDataNascimento() { return dataNascimento; }
+    public Boolean getAtivo() { return ativo; }
+    public void setNome(String nome) { this.nome = nome; }
+    public void setCpf(String cpf) { this.cpf = cpf; }
+    public void setEmail(String email) { this.email = email; }
+    public void setTelefone(String telefone) { this.telefone = telefone; }
+    public void setDataNascimento(LocalDate dataNascimento) { this.dataNascimento = dataNascimento; }
+    public void setAtivo(Boolean ativo) { this.ativo = ativo; }
 }
